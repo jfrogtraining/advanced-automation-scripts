f7a26b11ccded9547741f9df79465405fa9e14f0
Mark Galpin <markg@jfrog.com>
2016-05-19 16:45:15 -0700

    First cut at windows devenv config script

---
+++ wdevenv.bat
@@ -0,0 +1,7 @@
+mkdir artifactory-user-plugins-devenv\local-store\artifactory-pro\artifactory-pro-4.7.7\etc\plugins
+copy artifactory-user-plugins\storage\storageProperties\storageProperties.groovy artifactory-user-plugins-devenv\local-store\artifactory-pro\artifactory-pro-4.7.7\etc\plugins
+copy artifactory-user-plugins\config\repoLayoutsConfig\repoLayoutsConfig.groovy artifactory-user-plugins-devenv\local-store\artifactory-pro\artifactory-pro-4.7.7\etc\plugins
+copy artifactory-user-plugins-devenv\local-store\artifactory.lic artifactory-user-plugins-devenv\local-store\artifactory-pro\artifactory-pro-4.7.7\etc
+copy artifactory-user-plugins-devenv\etc\server.xml artifactory-user-plugins-devenv\local-store\artifactory-pro\artifactory-pro-4.7.7\tomcat\conf
+copy artifactory-user-plugins-devenv\etc\artifactory.system.properties artifactory-user-plugins-devenv\local-store\artifactory-pro\artifactory-pro-4.7.7\etc
+artifactory-user-plugins-devenv\local-store\artifactory-pro\artifactory-pro-4.7.7\bin\artifactory.bat
